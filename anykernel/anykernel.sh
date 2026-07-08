be8db60f5cac6b9cc9164217c224177306f52e45
ZiYue-yue <925988492@qq.com>
2026-07-08 20:20:10 +0800

    Create anykernel.sh

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,44 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=cezanne Kernel with LXC/Docker/KernelSU
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=cezanne
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; }
+
+# shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel boot install
+dump_boot;
+
+# 注入内核 cmdline 可选参数（如需要）
+# cmdline参数根据需要添加，一般不需要额外修改
+
+write_boot;
+## end boot install
